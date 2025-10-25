2596f9e147615a866cebad1c751dffd8d6a02a1a
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-25 23:00:06 +0200

    validación de email duplicado

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -24,7 +24,7 @@ const registrar=async(req,res)=>{
     await check('nombre').notEmpty().withMessage('El campo nombre es obligatorio').run(req);
     await check('email').isEmail().withMessage('El email no es válido').run(req);
     await check('password').isLength({min:6}).withMessage('El password debe ser de al menos 6 caracteres').run(req);
-    await check('repetir_password').equals('password').withMessage('Los passwords deben ser iguales').run(req);
+    //await check('repetir_password').equals('password').withMessage('Los passwords deben ser iguales').run(req);
 
     let resultado=validationResult(req);
 
@@ -39,7 +39,19 @@ const registrar=async(req,res)=>{
         }
     });
     }
-    //res.json(resultado.array());
+
+    //verificar que el email no esté duplicado
+    const existeUsuario=await Usuario.findOne({where:{email:req.body.email}});
+    if(existeUsuario){
+        return res.render('auth/registro',{
+        pagina:'Crear Cuenta',
+        errores: [{msg:'El email ya está registrado'}],
+        usuario:{
+            nombre:req.body.nombre,
+            email:req.body.email
+        }
+    });
+    }
 
     const usuario=await Usuario.create(req.body);
     res.json(usuario);
