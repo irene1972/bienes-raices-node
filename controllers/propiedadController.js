f163770c1b9198970edf717dcb75f099a335f4bd
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-26 15:39:55 +0100

    añadir funcionalidad crear propiedad

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -5,6 +5,14 @@ const admin=(req,res)=>{
     });
 }
 
+const crear=(req,res)=>{
+    res.render('propiedades/crear',{
+        pagina:'Crear Propiedad',
+        barra:true
+    });
+}
+
 export {
-    admin
+    admin,
+    crear
 }
